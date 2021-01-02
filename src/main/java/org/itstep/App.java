13a364e0255c31b7f9eef3b3758161348fd34498
ANN <vbkrfvjz1@gmail.com>
2021-01-02 15:17:25 +0200

    done

--- src/main/java/org/itstep/App.java
+++ src/main/java/org/itstep/App.java
@@ -3,36 +3,133 @@ package org.itstep;
 import javafx.animation.FadeTransition;
 import javafx.animation.ScaleTransition;
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.PointLight;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-import java.io.File;
-import java.io.InputStream;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class App extends Application {
-
+public static final Paint BLACK=Paint.valueOf( "#000000" );
+    public static final Paint WHITE =Paint.valueOf( "#ffffff" );
+    public static final Font LABEL_FONT=Font.font("Roboto", FontWeight.BOLD, 18 );
     @Override
     public void start(Stage stage) {
-        ImageView logoView = new ImageView();
+        //ImageView logoView = new ImageView();
 
-        Image image = new Image(pathToCard("itstep-logo.png"));
+        //Image image = new Image(pathToCard("itstep-logo.png"));
 
-        logoView.setImage(image);//
-        logoView.setPreserveRatio(true);
-        logoView.setFitWidth(100);
+        //logoView.setImage(image);//
+        //logoView.setPreserveRatio(true);
+        //logoView.setFitWidth(100);
 
-        playAnimation(logoView);
+        //playAnimation(logoView);
+        //корневой узел
+Pane root=new Pane();
+Rectangle greeenRect =new Rectangle(530,600);//создане прямоугольника
+        greeenRect.setFill( Paint.valueOf("#249477") );
 
-        Scene scene = new Scene(new StackPane(logoView), 640, 480);
+        Rectangle blackRect=new Rectangle(270,600);
+        blackRect.setFill( BLACK );
+        blackRect.relocate( 530,0 );
+
+//метка-текст
+Label dealerlabel=new Label("Dealer: ");
+dealerlabel.setFont(  LABEL_FONT );
+dealerlabel.setTextFill(WHITE);
+dealerlabel.relocate( 153,15 );
+dealerlabel.setPrefWidth( 194 );
+dealerlabel.setAlignment( Pos.CENTER );
+
+
+Label blackJackLabel=new Label("Black Jack");
+blackJackLabel.setTextFill( WHITE );
+        blackJackLabel.relocate( 135,272 );
+        blackJackLabel.setFont( Font.font( "Robot", FontWeight.MEDIUM, 48  ) );
+
+        Label playerLabel=new Label("Player: ");
+        playerLabel.setTextFill( WHITE );
+        playerLabel.setFont( LABEL_FONT );playerLabel.relocate( 156,565 );
+
+        playerLabel.setPrefWidth( 217 );
+        playerLabel.setAlignment( Pos.CENTER );
+
+Label cashLabel=new Label("Cash: ");
+cashLabel.setTextFill( WHITE );
+cashLabel.setFont(Font.font( "Robot",FontWeight.MEDIUM,24 ));
+cashLabel.relocate( 603,11 );
+cashLabel.setPrefWidth( 122 );
+cashLabel.setAlignment( Pos.CENTER );
+
+Label labelbet=new Label("BET");
+        labelbet.setFont(  LABEL_FONT );
+        labelbet.setTextFill(WHITE);
+        labelbet.relocate( 573,308 );
+        labelbet.setPrefWidth( 179 );
+        labelbet.setAlignment( Pos.CENTER );
+        //buttons
+        Button buttonhit=new Button("HIT");
+        buttonhit.setBackground( new Background( new BackgroundFill( Paint.valueOf( "#C4C4C4" ),null,null ) ) );
+        buttonhit.setFont(LABEL_FONT);
+        buttonhit.setPrefWidth( 190 );
+        buttonhit.setPrefHeight( 40 );
+        buttonhit.relocate( 572,156 );
+
+        Button buttonstand=new Button("STAND");
+        buttonstand.setBackground( new Background( new BackgroundFill( Paint.valueOf( "#C4C4C4" ),null,null ) ) );
+        buttonstand.setFont(LABEL_FONT);
+        buttonstand.setPrefWidth( 190 );
+        buttonstand.setPrefHeight( 40 );
+        buttonstand.relocate( 572,236 );
+
+        Button buttonplay=new Button("PLAY");
+        buttonplay.setBackground( new Background( new BackgroundFill( Paint.valueOf( "#C4C4C4" ),null,null ) ) );
+        buttonplay.setFont(LABEL_FONT);
+        buttonplay.setPrefWidth( 190 );
+        buttonplay.setPrefHeight( 40 );
+        buttonplay.relocate( 572,537 );
+
+
+        //pole vvoda
+        TextField textField=new TextField("100");
+        textField.setPrefWidth( 190 );
+        textField.setPrefHeight( 54 );
+        textField.setAlignment( Pos.CENTER );
+        textField.setFont(Font.font( "Robot",FontWeight.MEDIUM,24 )  );
+        textField.relocate( 572,368 );
+
+//добавить метку в корневой узел
+        root.getChildren().add( greeenRect );
+        root.getChildren().add(blackRect);
+root.getChildren().add( dealerlabel );
+root.getChildren().add(blackJackLabel);
+root.getChildren().add(playerLabel);
+root.getChildren().add( cashLabel );
+root.getChildren().add(buttonhit);
+        root.getChildren().add(buttonstand);
+        root.getChildren().add(buttonplay);
+        root.getChildren().add(textField);
+        root.getChildren().add(labelbet);
+
+//создать сцену
+        Scene scene = new Scene(root, 800, 600);
+        //установить сцену в окне
+        stage.setResizable( false );//фиксируем размер сцены
         stage.setScene(scene);
         stage.setTitle("Simple application in JavaFX");
         stage.getIcons().add(new Image("icon.png"));
